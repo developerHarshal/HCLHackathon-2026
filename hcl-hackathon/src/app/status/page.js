ba5114f72057048ea11d4e3ba35586e654acda4f
neetachavan25079 <neetachavan25079@gmail.com>
2026-04-18 15:52:23 +0530

    Update page.js

--- hcl-hackathon/src/app/status/page.js
+++ hcl-hackathon/src/app/status/page.js
@@ -1,3 +1,15 @@
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import Container from '@mui/material/Container';
+
 export default function ApplicationStatusCheckPage() {
-  return <div>Application Status Check</div>;
+  return <div>
+    <Container maxWidth="sm">
+    <h1>Application Status</h1><br></br>
+     <TextField id="outlined-basic" label="Application number" variant="outlined" /><br></br>
+     <TextField id="outlined-basic" label="Contact No" variant="outlined" /><br></br>
+      <TextField id="outlined-basic" label="Pan no" variant="outlined" /><br></br>
+      <Button variant="contained" href="#">Submit</Button>
+      </Container>
+    </div>;
 }
