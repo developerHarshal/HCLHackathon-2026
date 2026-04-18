7e958eabc6060af1f0c0a3d4fb769459531c9784
sushil151 <sushil@dotanddash.in>
2026-04-18 16:32:04 +0530

    dashboard changes

--- hcl-hackathon/src/component/dashboard.js
+++ hcl-hackathon/src/component/dashboard.js
@@ -23,6 +23,7 @@ const Dashboard = () => {
   const [data, setData] = useState(null);
   const [open, setOpen] = useState(false);
   const [rowData, setRowData] = useState(null);
+  const [creditScore, setCreditScore] = useState(null);
 
   const handleLogout = () => {
     console.log("In handleLogout");
@@ -36,12 +37,19 @@ const Dashboard = () => {
     setOpen(true);
   }
 
+  const handleCreditScore = (rowData) => {
+    console.log("In handleCreditScore");
+    setCreditScore(rowData.creditScore);
+  }
+
   useEffect(() => {
     setData([
-      { id: 1001, name: "User1 User1", contactNo: "9876543210", panNo: "ABCDE1234F", status: "Pending" },
-      { id: 1002, name: "User2 User2", contactNo: "9876543211", panNo: "ABCDE1234G", status: "Pending" },
-      { id: 1003, name: "User3 User3", contactNo: "9876543210", panNo: "ABCDE1234H", status: "Pending" },
-      { id: 1004, name: "User4 User4", contactNo: "9876543211", panNo: "ABCDE1234I", status: "Pending" },
+      { id: 1001, name: "User1 User1", contactNo: "9876543210", panNo: "ABCDE1234F", status: "Pending", email: "abc@gmail.com", occupation: "Private Job", income: "190000", age: "21", creditScore: "810" },
+      { id: 1002, name: "User2 User2", contactNo: "9876543211", panNo: "ABCDE1234G", status: "Pending", email: "abc@gmail.com", occupation: "Private Job", income: "250000", age: "21", creditScore: "810" },
+      { id: 1003, name: "User3 User3", contactNo: "9876543210", panNo: "ABCDE1234H", status: "Pending", email: "abc@gmail.com", occupation: "Private Job", income: "400000", age: "21", creditScore: "810" },
+      { id: 1004, name: "User4 User4", contactNo: "9876543211", panNo: "ABCDE1234I", status: "Pending", email: "abc@gmail.com", occupation: "Private Job", income: "550000", age: "21", creditScore: "810" },
+      { id: 1004, name: "User4 User4", contactNo: "9876543211", panNo: "ABCDE1234I", status: "Pending", email: "abc@gmail.com", occupation: "Private Job", income: "550000", age: "17", creditScore: "810" },
+      { id: 1004, name: "User4 User4", contactNo: "9876543211", panNo: "ABCDE1234I", status: "Pending", email: "abc@gmail.com", occupation: "Private Job", income: "550000", age: "21", creditScore: "750" },
     ]);
   }, [])
 
@@ -101,31 +109,61 @@ const Dashboard = () => {
             left: 200
           }}
         >
+          <Typography variant="h4" sx={{ margin: "10px 0 10px 135px" }}>Application: {rowData?.id}</Typography>
           <Table>
             <TableBody>
               <TableRow>
-                <TableCell>
-                  ID: {rowData?.id}
-                </TableCell>
-                <TableCell>
+                <TableCell sx={{ border: "none"}}>
                   Name: {rowData?.name}
                 </TableCell>
-                <TableCell>
+                <TableCell sx={{ border: "none"}}>
                   Contact No: {rowData?.contactNo}
+                </TableCell>
+                <TableCell sx={{ border: "none"}}>
+                  Email: {rowData?.email}
                 </TableCell>
               </TableRow>
               <TableRow>
-                <TableCell>
+                <TableCell sx={{ border: "none"}}>
                   PAN NO: {rowData?.panNo}
                 </TableCell>
-                <TableCell>
-                  Status: {rowData?.status}
+                <TableCell sx={{ border: "none"}}>
+                  Occupation: {rowData?.occupation}
                 </TableCell>
-                <TableCell>
+                <TableCell sx={{ border: "none"}}>
+                  Annual Incom: {rowData?.income}
+                </TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell sx={{ border: "none"}}>
+                  Age: {rowData?.age}
+                </TableCell>
+                <TableCell sx={{ border: "none"}}>
+                </TableCell>
+                <TableCell sx={{ border: "none"}}>
                 </TableCell>
               </TableRow>
             </TableBody>
           </Table>
+          <Container style={{ margin: "10px 0 10px 135px" }}>
+            <Button variant="contained" color="error" onClick={() => handleCreditScore(rowData)}>
+              Fetch Credit score
+            </Button>
+          </Container>
+          {
+            // creditScore ?
+            //   <Container style={{ margin: "10px 0 10px 135px" }}>
+            //     <TextField fullWidth label="Credit Score" margin="normal" disabled value={creditScore ? creditScore : ""} />
+            //     <TextField fullWidth label="Credit limit" margin="normal" />
+            //     <Button variant="contained">
+            //       Approve
+            //     </Button>
+            //     <Button variant="contained" color="error">
+            //       Reject
+            //     </Button>
+            //   </Container>
+            // : <></>
+          }
         </Box>
       </Modal>
     </>
